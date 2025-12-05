2057efc8f30b70e8899acc94eb066672b5501204
bartzbeielstein <32470350+bartzbeielstein@users.noreply.github.com>
2025-12-05 22:27:50 +0100

    0.0.61

--- src/spotoptim/mo/__init__.py
+++ src/spotoptim/mo/__init__.py
@@ -1,9 +1,23 @@
-from .pareto import is_pareto_efficient, plot_mo
-from .mo_mm import mo_mm_desirability_function, mo_mm_desirability_optimizer
+from .pareto import (
+    is_pareto_efficient,
+    plot_mo,
+    mo_xy_contour,
+    mo_xy_surface,
+    mo_pareto_optx_plot,
+)
+from .mo_mm import (
+    mo_mm_desirability_function,
+    mo_mm_desirability_optimizer,
+    mo_xy_desirability_plot,
+)
 
 __all__ = [
     "is_pareto_efficient",
     "plot_mo",
     "mo_mm_desirability_function",
     "mo_mm_desirability_optimizer",
+    "mo_xy_contour",
+    "mo_xy_surface",
+    "mo_xy_desirability_plot",
+    "mo_pareto_optx_plot",
 ]
